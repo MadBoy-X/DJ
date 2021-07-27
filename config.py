cb025b2def709daa3a14b495befc5de29f21eb98
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-07-27 09:55:30 +0530

    Initial Commit

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ PLAY_PIC = getenv("PLAY_PIC")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! ;").split())
 
